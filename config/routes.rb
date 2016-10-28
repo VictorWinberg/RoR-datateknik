9e5ded9277df3f1dad30fdd3f4ae9d55317948bf
VictorWinberg <dat14vwi@student.lu.se>
2016-10-28 12:42:51 +0200

    rails g controller pages home secure + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,21 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/secure'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # Root of your site with "root"
+  root 'pages#home'
 
-  # Example of regular route:
+  # Regular routes:
   #   get 'products/:id' => 'catalog#view'
+  get 'pages/secure'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
+  # Named routes, that can be invoked with purchase_url(id: product.id):
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  # Resource routes (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  # Example resource route with options:
+  # Resource routes with options:
   #   resources :products do
   #     member do
   #       get 'short'
@@ -30,13 +27,13 @@ Rails.application.routes.draw do
   #     end
   #   end
 
-  # Example resource route with sub-resources:
+  # Resource routes with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
   #     resource :seller
   #   end
 
-  # Example resource route with more complex sub-resources:
+  # Resource routes with more complex sub-resources:
   #   resources :products do
   #     resources :comments
   #     resources :sales do
@@ -44,14 +41,14 @@ Rails.application.routes.draw do
   #     end
   #   end
 
-  # Example resource route with concerns:
+  # Resource routes with concerns:
   #   concern :toggleable do
   #     post 'toggle'
   #   end
   #   resources :posts, concerns: :toggleable
   #   resources :photos, concerns: :toggleable
 
-  # Example resource route within a namespace:
+  # Resource routes within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
