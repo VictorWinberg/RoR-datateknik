1a2e829c11de159d0397ebd0d4734b2bb968afca
VictorWinberg <dat14vwi@student.lu.se>
2016-10-28 16:54:28 +0200

    Website update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,16 +1,4 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # def google_oauth2
-  #   @user = User.from_omniauth(request.env["omniauth.auth"])
-  #
-  #   if @user.persisted?
-  #     sign_in_and_redirect @user, :event => :authentication # this will throw if @user is not activated
-  #     set_flash_message(:notice, :success, :kind => "Google OAuth2") if is_navigational_format?
-  #   else
-  #     session["devise.google_oauth2_data"] = request.env["omniauth.auth"]
-  #     redirect_to new_user_registration_url
-  #   end
-  # end
-
   def self.provides_callback_for(provider)
     class_eval %Q{
       def #{provider}
