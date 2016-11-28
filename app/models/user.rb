d476103db800b05a69bea2a333a180bdc08f3125
VictorWinberg <dat14vwi@student.lu.se>
2016-11-29 00:31:11 +0100

    CAS email added in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      # user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
+      user.email = auth.info.email
+      if auth.provider == :cas || auth.provider == :cas3
+        user.email = user.name + "@student.lu.se"
+      end
+      # user.password = Devise.friendly_token[0,20]
       # user.image = auth.info.image
     end
   end
