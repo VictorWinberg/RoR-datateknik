b238f0492ae0ed1834082a941fd91e1a1f7a1950
VictorWinberg <dat14vwi@student.lu.se>
2016-12-14 12:38:06 +0100

    Mailer url fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,13 +22,13 @@ Rails.application.configure do
     port: 587,
     domain: "gmail.com",
     authentication: "plain",
-    #enable_starttls_auto: true,
+    enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
   }
 
   # Default url
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
