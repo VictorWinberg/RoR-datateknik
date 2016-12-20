0543b2dd70cb4eb6ee79e4367e1613f871d413d6
VictorWinberg <dat14vwi@student.lu.se>
2016-12-20 17:36:53 +0100

    Turbolink fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require_tree .
-$(function () {
+$(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip()
 })
